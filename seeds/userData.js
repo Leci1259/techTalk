a57d7e1330c0f25c6bc96972f8ffc1e47e0d123c
Leci1259 <lreid31@gatech.edu>
2021-10-29 22:51:04 -0400

    added seeds js files; fixed typos; added scripts to json

---
+++ seeds/userData.js
@@ -0,0 +1,30 @@
+const { User } = require('../models');
+
+
+const userData = [
+    {
+      "username": "leci@gmail.com",
+      "password": "pass1234"
+    },
+    {
+      "username": "kp@gmail.com",
+      "password": "pass1234"
+    },
+    {
+      "username": "rt@yahoo.com",
+      "password": "pass1234"
+    },
+    {
+      "username": "lukey@yahoo.com",
+      "password": "pass1234"
+    },
+    {
+      "username": "klettso@comcast.net",
+      "password": "pass1234"
+    }
+  ];
+  
+
+const seedData = () => User.bulkCreate(userData);
+
+module.exports = seedData;
