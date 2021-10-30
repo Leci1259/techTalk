1a50e6d9dd04549af5c52ba6540bc2bf47ff2886
Leci1259 <lreid31@gatech.edu>
2021-10-30 12:35:17 -0400

    fixing error; forgot to add index.js inside api folder for connections

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 const path = require('path');
 const express = require("express");
-const routes = require("./controllers");
-const sequelize = require('./config/connection');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const helpers = require("./utils/helpers");
+const routes = require("./controllers/");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create;
+const hbs = exphbs.create({helpers});
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
 
 //Express.js handlebar connection
 app.engine('handlebars', hbs.engine);
-app.set('view engine', '.handlebars');
+app.set('view engine', 'handlebars');
 
 //middleware
 app.use(express.json());
