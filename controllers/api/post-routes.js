2bff0098446b018de9f05de11a4d6f5db11b7585
Leci1259 <lreid31@gatech.edu>
2021-10-29 21:28:10 -0400

    working on api routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { Post } = require('../../models/');
+const withAuth = require('../../utils/auth');
+
+//new post
+
+router.post('/',withAuth, async (req,res) => {
+    try{
+        const body = req.body;
+        const newPost = await Post.create({body,userid: req.session.userid});
+        res.status(200).json(newPost)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//update
+router.put('/:id',withAuth, async (req,res) => {
+try{
+    const check = await Post.update(req.body, {
+        where: { id: req.params.id},
+    });
+
+    if (check > 0){
+        res.status(200).end();
+    }
+    else {
+        res.status(400).end();
+    }
+
+} catch(err) {
+    res.status(500).json(err);
+}
+})
+//delete
+router.delete('/:id',withAuth, async (req,res) => {
+    try{
+        const check = await Post.destroy({
+            where: { id: req.params.id},
+        });
+
+        if (check > 0){
+            res.status(200).end();
+        }
+        else {
+            res.status(400).end();
+        }
+
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,43 @@
 const router = require('express').Router();
-const { Art, Artist, User, Cart } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 // Home Page Route
 router.get('/', async (req, res) => {
   try {
-  
+    const postData = await Post.findAll({
+      include: User,
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      
+      posts
     });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// get post by id
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render('postpage', { post });
+    } else {
+      res.status(400).json({ message: 'Post Not Found' });
+    }
   } catch (err) {
     res.status(500).json(err);
   }
@@ -31,18 +59,37 @@ router.get('/dashboard',withAuth, async (req, res) => {
 // Login Page Route
 router.get('/login', async (req, res) => {
   try {
-    // Pass serialized data and session flag into template
+    if( req.session.loggedIn) {
+      res.redirect('/')
+      return;
+    }
     res.render('login');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// Signup Page Route
+router.get('/signup', async (req, res) => {
+  try {
+    if( req.session.loggedIn) {
+      res.redirect('/')
+      return;
+    }
+    res.render('signup');
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // Logout Page Route
-router.get('/logout', withAuth, async (req, res) => {
+router.get('/logout', async (req, res) => {
   try {
-    // Pass serialized data and session flag into template
-    res.render('logout');
+    if (!req.session.loggedIn) {
+      res.render('logout');
+    }
+    else {
+      res.status(400).json({ message: 'Error logging you out' });
+    }
   } catch (err) {
     res.status(500).json(err);
   }
