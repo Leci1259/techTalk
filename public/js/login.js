8584d258962e36af4e69b00420b224f4096aac0d
Leci1259 <lreid31@gatech.edu>
2021-11-03 15:43:52 -0400

    fixed login/sign up error

--- public/js/login.js
+++ public/js/login.js
@@ -1,31 +1,34 @@
-const loginFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const usernameEl = $('.user-name').val();
-    const passwordEl = $('.user-password').val();
-  
-    const response = await fetch('/api/user/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        username: usernameEl,
-        password: passwordEl,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to login');
-    }
-  };
-  
-  const switchToSignup = function () {
-    document.location.replace('/signup');
+const loginFormHandler = async function (event) {
+  event.preventDefault();
 
+  const usernameEl = $('.user-name').val();
+  const passwordEl = $('.user-password').val();
 
+  const response = await fetch('/api/user/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: usernameEl,
+      password: passwordEl,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to login');
   }
-  document
-    $('.loginbutton').on('click',loginFormHandler)
-    $('.switch_signup').on('click',switchToSignup)
-  
+};
+
+const switchToSignup = function () {
+  document.location.replace('/signup');
+
+
+}
+document
+  .querySelector('.loginbutton')
+  .addEventListener('click', loginFormHandler)
+document
+  .querySelector('.switch-signup')
+  .addEventListener('click', switchToSignup)
+
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,26 +1,26 @@
-const signupFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const usernameEl = $('.user-name').val();
-    const passwordEl = $('.user-password').val();
-  
-    const response = await fetch('/api/user', {
-      method: 'POST',
-      body: JSON.stringify({
-        username: usernameEl,
-        password: passwordEl,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to sign up');
-    }
-  };
-  
-  document
-  $('.signupbutton').on('click',signupFormHandler)
+const signupFormHandler = async function (event) {
+  event.preventDefault();
 
-  
+  const usernameEl = $('.user-name').val();
+  const passwordEl = $('.user-password').val();
+
+  const response = await fetch('/api/user', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: usernameEl,
+      password: passwordEl,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to sign up');
+  }
+};
+
+document
+  .querySelector('.signupbutton')
+  .addEventListener('click', signupFormHandler)
+
