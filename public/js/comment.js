d8be01bdf298d4efda7867d784ce39048402f88b
Leci1259 <lreid31@gatech.edu>
2021-11-03 16:16:20 -0400

    trying to fix comment function

--- public/js/comment.js
+++ public/js/comment.js
@@ -3,20 +3,22 @@ const commentHandler = async function (event) {
 
     const button = event.target
     const commentText = $(button).prev().val()
-    const postId = $(button).siblings().eq(1).children().eq(0).val();
+    const postId = $(button).siblings().eq(1).children().eq(0).val().trim();
 
     if (commentText) {
-        await fetch('/api/comment', {
+        const response = await fetch('/api/comment', {
             method: 'POST',
             body: JSON.stringify({
+                commentText,
                 postId,
-                commentText
             }),
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-        document.location.reload();
+        if (response.ok) {
+            document.location.reload();
+        }
     };
 }
 
